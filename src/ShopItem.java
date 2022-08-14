123e2b86726c4167bca8cacebca6bf59bd55f4f6
Kevin Chevalier <kevin.chevalier10@gmail.com>
2022-08-13 17:04:30 -1000

    Max Shop Purchasing
    Closes #38

--- src/ShopItem.java
+++ src/ShopItem.java
@@ -8,12 +8,17 @@ public class ShopItem extends ShopEntity{
 
     public int price;
     public int quantity;
+    public int max_quantity;
 
     public ShopItem(JSONObject o){
         super(o);
-
+        
         this.price = Utils.castInt(o.get("price"));
         this.quantity = Utils.castInt(o.get("quantity"));
+        
+        Object max_quantity = o.get("max_quantity");
+        this.max_quantity = max_quantity == null ? Integer.MAX_VALUE / 2 : Utils.castInt(max_quantity);
+
     }
 
     public static ShopItem get(String id){
